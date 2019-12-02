802e7a5011e3a5052531a5fa38f34a0e1c5105ff
thompsonnaidu <thompsonnaiducm1343@gmail.com>
2019-12-02 09:27:47 +0530

    Creating database connection and creating registration routes

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,49 @@
 const express=require('express');
-
 const router=express.Router();
+const bcrypt=require('bcryptjs')
+const { check, validationResult } = require('express-validator');
+
+const User=require('../models/Users')
 
 // @route POST api/users
 // @desc Register a user
 // @access Public
-router.post('/',(req,res)=>{
-    res.send("Register a user")
+router.post('/',[
+    check("name","Name is required").not().isEmpty(),
+    check("email","Please include valid email").isEmail(),
+    check("password","Please enter password with 8 or more character").isLength({ min :6 })
+], async (req,res)=>{
+
+    const errors=validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors:errors.array() });
+    }
+    
+    const {email,name,password}=req.body;
+    try{
+        let user = await User.findOne({ email });
+
+        //Checking if there is an existing register email
+        if(user){
+            return res.status(400).json({msg:"User already exits"})
+        }
+
+        user= new User({
+            name,
+            email,
+            password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+        user.password= await bcrypt.hash(password,salt);
+        await user.save();
+        res.send("user save")
+
+    }catch(err){
+        console.log("I amd in ",err)
+        res.status(500).send("server error")
+    }
+
 });
 
 
--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express =require('express');
 const app=express();
-
+const connectDB=require('./config/db');
 const PORT= process.env.PORT || 5000;
+//connect to Database
+connectDB()
+
+
+// initialise middleware
+app.use(express.json({ extended: false }));
 
 // Define Routes
 app.use('/api/users',require('./routes/users'))
 app.use('/api/contacts',require('./routes/contact'))
 app.use('/api/auth',require('./routes/auth'))
 
+
+
+
+// Start the server @ port 
 app.listen(PORT,()=>{
     console.log(`server started at ${PORT}`);
 })
