5db7dd9dfc50c9b3d6a6794f93db46438c2dcf4c
thompsonnaidu <thompsonnaiducm1343@gmail.com>
2019-12-04 08:33:59 +0530

    Implementing the client application for Contact Keeper and router to home page and about us page

---
+++ client/src/components/layouts/Navbar.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const Navbar = ({ title ,icon }) =>{
+
+    return (
+        <nav className="navbar navbar-expand-md bg-primary navbar-dark">
+            {/* <!-- Brand --> */}
+            <a className={icon+" navbar-brand"} href="#"> {title}</a>
+
+            {/* <!-- Toggler/collapsibe Button --> */}
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            {/* <!-- Navbar links --> */}
+            <div className="collapse navbar-collapse " id="collapsibleNavbar">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                    <Link className="nav-link" to="/about">About</Link>
+                    </li>
+                    <li className="nav-item">
+                    <Link className="nav-link" to="/">Home</Link>
+                    </li>
+                   
+                </ul>
+            </div>
+        </nav>
+
+        
+    );
+}
+
+Navbar.propTypes={
+    
+    title:PropTypes.string.isRequired,
+    icon:PropTypes.string.isRequired
+}
+
+Navbar.defaultProps={
+    title:"Contact Keeper",
+    icon : "fas fa-id-card-alt"
+}
+
+export default Navbar
+
+
+
---
+++ client/src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About=()=> {
+    return (
+        <div>
+            <div>
+                <h1>About This App</h1>
+                <p className=""> This is a full stack React application for keeping contact</p>
+                <p>Version:1.0.0</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default About
