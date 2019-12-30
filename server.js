0916607cce39b3bb13927f190b18c177a6d7b33e
thompsonnaidu <thompsonnaiducm1343@gmail.com>
2019-12-30 22:19:28 +0530

    prepare to deploy on heroku

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ if(process.env.Node_ENV === 'production'){
     app.use(express.static('client/build'));
 
     app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
 
     })
 } 
